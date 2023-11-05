6a20a6d564c166de57753dd1f7102a6138565d23
Lê  Hoàng Vũ <68913@thegioididong.com>
2023-11-05 11:19:25 +0700

    firt commit nike landing page with tailwincss and reactjs

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import SpecialOffer from './SpecialOffer';
+import Subcribe from "./Subcribe";
+import SuperQuality from "./SuperQuality";
+import Services from "./Services";
+
+export {
+    CustomerReviews,
+    Footer,
+    Hero,
+    PopularProducts,
+    SpecialOffer,
+    Subcribe,
+    SuperQuality,
+    Services,
+}
